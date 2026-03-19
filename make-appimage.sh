8ef4ef7619f9971acbf1fec945edfe50804eb9ed
Link4Electronics <link4electronics@gmail.com>
2026-03-19 20:48:42 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export STARTUPWMCLASS=
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/pcsx-redux
 
 # Additional changes can be done in between here
 
