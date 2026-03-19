04d47f3d4926d65c9fbd01c753a8593590659b3c
Link4Electronics <link4electronics@gmail.com>
2026-03-19 20:41:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,25 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm capstone fmt glfw libuv sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building PCSX-Redux..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/grumpycoders/pcsx-redux"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./pcsx-redux
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+mkdir -p ./AppDir/share/pcsx-redux/resources
+mkdir -p ./AppDir/share/pcsx-redux/fonts
+cd ./pcsx-redux
+make -j$(nproc)
+mv -v pcsx-redux ../AppDir/bin
+mv -v i18n ../AppDir/share/pcsx-redux
+mv -v third_party/noto/*.ttf third_party/noto/*.otf ../AppDir/share/pcsx-redux/fonts
+wget -O ../AppDir/share/pcsx-redux/resources/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
