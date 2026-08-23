172d7667edb1987cc7f3776c7fa2461a2e5e47a2
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:14:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libdecor \
-    sdl2
+pacman -Syu --noconfirm sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini
 
 # Comment this out if you need an AUR package
 make-aur-package pcsx-redux-git
