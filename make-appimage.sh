df3e845ef441fc6e773913bd9ae7660b7b151daa
Link4Electronics <link4electronics@gmail.com>
2026-08-26 10:37:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q pcsx-redux-git | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/pcsx-redux.png
-export DESKTOP=/usr/share/applications/pcsx-redux.desktop
+export DESKTOP=https://raw.githubusercontent.com/grumpycoders/pcsx-redux/refs/heads/main/resources/pcsx-redux.desktop
 export STARTUPWMCLASS=pcsx-redux
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/pcsx-redux
+quick-sharun ./AppDir/bin/pcsx-redux
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
