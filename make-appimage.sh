08e8a95f6613ed3262bf546479f3059bec924790
Link4Electronics <link4electronics@gmail.com>
2026-08-26 10:42:40 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,6 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/pcsx-redux.png
 export DESKTOP=https://raw.githubusercontent.com/grumpycoders/pcsx-redux/refs/heads/main/resources/pcsx-redux.desktop
 export STARTUPWMCLASS=pcsx-redux
 export DEPLOY_OPENGL=1
