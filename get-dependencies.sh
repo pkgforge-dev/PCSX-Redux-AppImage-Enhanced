a1055a667eb4ecbc2a9a5e6a3668d337b064fec1
Link4Electronics <link4electronics@gmail.com>
2026-08-27 18:34:11 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,19 +17,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package pcsx-redux-git
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
-
 echo "Building PCSX-Redux..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/grumpycoders/pcsx-redux"
